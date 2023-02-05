c2ba1681e0a303f6d63f6ed6d08676c32518af42
ArtemBorisov <artem.borisov2002@mail.ru>
2023-02-05 16:02:31 +0300

    Something done

--- project/include/utils.h
+++ project/include/utils.h
@@ -1,9 +1,22 @@
 #define UTILS_H
 #ifndef UTILS_H
-
+#include <stdio.h>
+#include <stddef.h>
 #define filename "transaction.dat";
+typedef struct masterRecord {
+    int Number;
+    char Name[20];
+    char Surname[20];
+    char addres[30];
+    char TelNumber[15];
+    double indebtedness;
+    double credit_limit;
+    double cash_payments;
+} Data;
 
-// This comment is very useful
-printf("This message is very useful too")
+
+void masterWrite(FILE *ofPTR, Data Client);
+void transactionWrite(FILE *ofPTR, Data transfer);
+void blackRecord(FILE *ofPTR, FILE *ofPTR_2, FILE *blackrecord, Data client_data, Data transfer);
 
 #endif //UTILS_H
