6a2dfbe8c76b877c1133570fe65ee8f1dc4b66b9
ArtemBorisov <artem.borisov2002@mail.ru>
2023-06-20 23:51:06 +0300

    HUUURRRAAAHH I've done third Homeworkgit add .! It's been so hard

--- project/src/matrix.c
+++ project/src/matrix.c
@@ -20,10 +20,11 @@ Matrix* create_matrix_from_file(const char* path_file) {
     Matrix *return_matrix = create_matrix(rows, cols);
     if (return_matrix == NULL) {
         puts(INVALID_STATUS_CODE);
+        fclose(opened_file);
         return NULL;
     }
-    for (int i = 0; i < rows * cols; i++) {
-        if (fscanf(opened_file, "%lf", matrix->data_arr[i]) == -1) {
+    for (size_t i = 0; i < rows * cols; i++) {
+        if (fscanf(opened_file, "%lf", &return_matrix->data_arr[i]) == -1) {
             puts(STATUS_CODE_ERROR);
         }
     }
@@ -37,14 +38,12 @@ Matrix* create_matrix(size_t rows, size_t cols) {
     Matrix *matrix = malloc(sizeof(Matrix));
     if (matrix == NULL) {
         puts(INVALID_STATUS_CODE);
-        //free(matrix);
         return NULL;
     }
     matrix->data_arr = calloc(rows * cols, sizeof(double));
     if (matrix->data_arr == NULL) {
         puts(INVALID_STATUS_CODE);
-        //free(matrix->data_arr);
-        //free(matrix);
+        free(matrix);
         return NULL;
     }
     matrix->rows = rows;
@@ -75,7 +74,7 @@ int get_cols(const Matrix* matrix, size_t* cols) {
     *cols = matrix->cols;
     return 0;
 }
-int get_elem(const Matrix* matrix, int row, int col, double* val) {
+int get_elem(const Matrix* matrix, size_t row, size_t col, double* val) {
     if (matrix == NULL) {
         puts(INVALID_STATUS_CODE);
         return 1;
@@ -83,7 +82,7 @@ int get_elem(const Matrix* matrix, int row, int col, double* val) {
     *val = matrix->data_arr[row * matrix->cols + col];
     return 0;
 }
-int set_elem(Matrix* matrix, int row, int col, double val) {
+int set_elem(Matrix* matrix, size_t row, size_t col, double val) {
     if (matrix == NULL) {
         puts(INVALID_STATUS_CODE);
         return 1;
@@ -96,27 +95,35 @@ Matrix* mul_scalar(const Matrix* matrix, double val) {
         puts(INVALID_STATUS_CODE);
         return NULL;
     }
-    for (int i = 0; i < matrix->rows * matrix->cols; i++) {
-        matrix->data_arr[i] *= val;
+    Matrix *new_matrix = create_matrix(matrix->rows, matrix->cols);
+    if (val == 0.) {
+        return new_matrix;
     }
-    return matrix;
+    for (size_t i = 0; i < matrix->rows; i++) {
+        for (size_t j = 0; j < matrix->cols; j++) {
+            double elem = 0.;
+            get_elem(matrix, i, j, &elem);
+            set_elem(new_matrix, i, j, val * elem);
+        }
+    }
+    return new_matrix;
 }
 Matrix* transp(const Matrix* matrix) {
     if (matrix == NULL) {
         puts(INVALID_STATUS_CODE);
         return NULL;
     }
-    double tmp = 0;
-    for (int i = 0; i < matrix->rows; i++) {
-        for (int j = 0; j < matrix->cols; j++) {
-            if (j > i) {
-                tmp = matrix->data_arr[i * matrix->cols + j];
-                matrix->data_arr[i * matrix->cols + j] = matrix->data_arr[j * matrix->rows + i];
-                matrix->data_arr[j * matrix->rows + i] = tmp;
-            }
+    size_t rows = matrix->cols;
+    size_t cols = matrix->rows;
+    Matrix* new_matrix = create_matrix(rows, cols);
+    for (size_t i = 0; i < matrix->rows; i++) {
+        for (size_t j = 0; j < matrix->cols; j++) {
+            double elem = 0.;
+            get_elem(matrix, i, j, &elem);
+            set_elem(new_matrix, j, i, elem);
         }
     }
-    return matrix;
+    return new_matrix;
 }
 Matrix* sum(const Matrix* l, const Matrix* r) {
     if (l == NULL || r == NULL) {
@@ -128,7 +135,7 @@ Matrix* sum(const Matrix* l, const Matrix* r) {
         return NULL;
     }
     Matrix* matrix = create_matrix(l->rows, l->cols);
-    for (int i = 0; i < matrix->rows * matrix->cols; i++) {
+    for (size_t i = 0; i < matrix->rows * matrix->cols; i++) {
         matrix->data_arr[i] = l->data_arr[i] + r->data_arr[i];
     }
     return matrix;
@@ -143,7 +150,7 @@ Matrix* sub(const Matrix* l, const Matrix* r) {
         return NULL;
     }
     Matrix* matrix = create_matrix(l->rows, l->cols);
-    for (int i = 0; i < matrix->rows * matrix->cols; i++) {
+    for (size_t i = 0; i < matrix->rows * matrix->cols; i++) {
         matrix->data_arr[i] = l->data_arr[i] - r->data_arr[i];
     }
     return matrix;
@@ -159,12 +166,11 @@ Matrix* mul(const Matrix* l, const Matrix* r) {
     }
     double eleml = 0.;
     double elemr = 0.;
-    double total = 0.;
     Matrix* matrix = create_matrix(l->rows, r->cols);
-    for (int i = 0; i < l->rows; i++) {
-        for (int j = 0; j < r->cols; j++) {
-            total = 0.;
-            for (int k = 0; k < l->cols; k++) {
+    for (size_t i = 0; i < l->rows; i++) {
+        for (size_t j = 0; j < r->cols; j++) {
+            double total = 0.;
+            for (size_t k = 0; k < l->cols; k++) {
                 get_elem(l, i, k, &eleml);
                 get_elem(r, k, j, &elemr);
                 total += eleml * elemr;
@@ -174,6 +180,51 @@ Matrix* mul(const Matrix* l, const Matrix* r) {
     }
     return matrix;
 }
+double ret_elem(const Matrix *matrix, size_t row, size_t col) {
+    double *val = matrix->data_arr + (row * matrix->cols + col);
+    return *val;
+}
+Matrix *del_row_col(const Matrix *matrix, size_t row_p, size_t col_p) {
+    size_t rows = matrix->rows - 1;
+    size_t cols = matrix->cols - 1;
+    Matrix *new_matrix = create_matrix(rows, cols);
+    double *new_matrix_data_ptr = new_matrix->data_arr;
+    for (size_t i = 0; i < matrix->rows; i++) {
+        for (size_t j = 0; j < matrix->cols; j++) {
+            if (i != row_p && j != col_p) {
+                *new_matrix_data_ptr = ret_elem(matrix, i, j);
+                new_matrix_data_ptr++;
+            }
+        }
+    }
+    return new_matrix;
+}
+double return_det(const Matrix *matrix) {
+    if (matrix == NULL) {
+        puts(INVALID_STATUS_CODE);
+        return 0.;
+    }
+    if (matrix->rows == 1) {
+        return ret_elem(matrix, 0, 0);
+    } else if (matrix->rows == 2) {
+        double val = ret_elem(matrix, 0, 0) * ret_elem(matrix, 1, 1)
+                     - ret_elem(matrix, 0, 1) * ret_elem(matrix, 1, 0);
+        return val;
+    } else {
+        double answer = 0.;
+        double sign = 1.;
+        for (size_t i = 0; i < matrix->cols; i++) {
+            Matrix *minor = del_row_col(matrix, 0, i);
+            double mult = ret_elem(matrix, 0, i);
+            double buf = return_det(minor);
+            answer += sign * mult * buf;
+            sign *= -1.;
+            free_matrix(minor);
+        }
+        return answer;
+    }
+}
+
 int det(const Matrix* matrix, double* val) {
     if (matrix == NULL) {
         puts(INVALID_STATUS_CODE);
@@ -186,97 +237,38 @@ int det(const Matrix* matrix, double* val) {
     *val = return_det(matrix);
     return 0;
 }
-double return_det(const Matrix *matrix) {
-    if (matrix == NULL) {
-        puts(INVALID_STATUS_CODE);
-        return 0.;
-    }
-    if (matrix->rows == 1) {
-        return ret_elem(matrix, 0, 0);
-    }
-    else if (matrix->rows == 2) {
-        double val = ret_elem(matrix, 0, 0) * ret_elem(matrix, 1, 1)
-                - ret_elem(matrix, 0, 1) * ret_elem(matrix, 1, 0);
-        return val;
-    } else {
-        double answer = 0.;
-        double sign = 1.;
-        for (int i = 0; i < matrix->cols; i++) {
-            Matrix *minor = del_row_col(matrix, 0, i);
-            double mult = ret_elem(matrix, 0, i);
-            double buf = return_det(matrix);
-            answer += sign * mult * buf;
-            sign *= -1.;
-            free_matrix(minor);
-        }
-        return answer;
-    }
-}
-double ret_elem(Matrix *matrix, int row, int col) {
-    if (matrix == NULL) {
-        puts(INVALID_STATUS_CODE);
-        return 0.;
-    }
-    double *val = matrix->data_arr + (row * matrix->cols + col);
-    return *val;
-}
-Matrix *del_row_col(Matrix *matrix, int row_p, int col_p) {
-    if (matrix == NULL) {
-        puts(INVALID_STATUS_CODE);
-        return NULL;
-    }
-    int rows = matrix->rows - 1;
-    int cols = matrix->cols - 1;
-    Matrix *new_matrix = create_matrix(rows, cols);
-    for (int i = 0; i < matrix->rows; i++) {
-        for (int j = 0; j < matrix->cols; j++) {
-            if (i != row_p && j != col_p) {
-                *new_matrix->data_arr = ret_elem(matrix, i, j);
-                new_matrix->data_arr++;
-            }
-        }
-    }
-    return new_matrix;
-}
 
 Matrix* adj(const Matrix* matrix) {
-    if (matrix == NULL) {
+    if (matrix == NULL || matrix->rows != matrix->cols) {
         puts(INVALID_STATUS_CODE);
         return NULL;
+    } else if (matrix->cols < 2) {
+        return transp(matrix);
     }
-    if (matrix->cols < 2 && matrix->rows < 2) {
-        puts(STATUS_CODE_ERROR);
-        return NULL;
-    }
-    double val = 0.;
-    Matrix *minor = NULL;
     double determinant = 0.;
-    double comp_of_matrix = 0.;
-    double sign = 1.;
     Matrix *new_matrix = create_matrix(matrix->rows, matrix->cols);
-    for (int i = 0; i < matrix->rows; i++) {
-        for (int j = 0; j < matrix->cols; j++) {
-            val = ret_elem(matrix, i, j);
-            minor = del_row_col(matrix, i, j);
-            det(minor, determinant);
-            comp_of_matrix = sign * determinant;
-            set_elem(new_matrix, i, j, comp_of_matrix);
-            sign *= -1.;
+    for (size_t i = 0; i < matrix->rows; i++) {
+        for (size_t j = 0; j < matrix->cols; j++) {
+            Matrix *minor = del_row_col(matrix, i, j);
+            double sign = (i + j) % 2 == 0 ? 1. : -1.;
+            det(minor, &determinant);
+            set_elem(new_matrix, j, i, sign * determinant);
             free_matrix(minor);
         }
     }
     return new_matrix;
 }
 Matrix* inv(const Matrix* matrix) {
-    if (matrix == NULL) {
+    if (matrix == NULL || matrix->rows != matrix->cols) {
         puts(INVALID_STATUS_CODE);
         return NULL;
+    } else if (matrix->rows == 1) {
+        Matrix* inversed = create_matrix(1, 1);
+        set_elem(inversed, 0, 0, 1 / ret_elem(matrix, 0, 0));
+        return inversed;
     }
-    Matrix *adj_matrix = adj(matrix)
-    double determinant = 0.;
-    det(matrix, determinant);
-    determinant = 1 / determinant;
-    Matrix *inv_matrix = mul_scalar(adj_matrix, determinant);
+    Matrix *adj_matrix = adj(matrix);
+    Matrix *inv_matrix = mul_scalar(adj_matrix, (1 / return_det(matrix)));
     free_matrix(adj_matrix);
     return inv_matrix;
 }
